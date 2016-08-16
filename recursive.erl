fe9d75f07b0ce429ebc7c3e087958396667285b5
Long Tran <long.thanh.tran@gmail.com>
2016-08-16 22:46:30 +0700

    Follow qsort and tree parts.

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, len/1, tail_fac/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2, tail_zip/2, tail_lenient_zip/2]).
+-export([fac/1, len/1, tail_fac/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2, tail_zip/2, tail_lenient_zip/2, quicksort/1, partition/4, lc_quicksort/1, bestest_qsort/1, bestest_partition/4]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
@@ -63,3 +63,44 @@ tail_lenient_zip([], _, Acc) -> Acc;
 tail_lenient_zip(_, [], Acc) -> Acc;
 tail_lenient_zip([L1|L1s], [L2|L2s], Acc) ->
   tail_lenient_zip(L1s, L2s, [{L1,L2}|Acc]).
+
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  { Smaller, Larger } = partition(Pivot, Rest, [], []),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_, [], Smaller, Larger) -> { Smaller, Larger };
+partition(Pivot, [H|T], Smaller, Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+     H > Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
+
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+  lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
+  ++ [Pivot] ++
+  lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
+
+%% BESTEST QUICKSORT, YEAH!
+%% (This is not really the bestest quicksort, because we do not do
+%%  adequate pivot selection. It is the bestest of this book, alright?
+%%  Thanks to literateprograms.org for this example. Give them a visit!
+%%  http://en.literateprograms.org/Quicksort_(Erlang) )
+bestest_qsort([]) -> [];
+bestest_qsort(L=[_|_]) ->
+  bestest_qsort(L, []).
+
+bestest_qsort([], Acc) -> Acc;
+bestest_qsort([Pivot|Rest], Acc) ->
+  bestest_partition(Pivot, Rest, {[], [Pivot], []}, Acc).
+
+bestest_partition(_, [], {Smaller, Equal, Larger}, Acc) ->
+  bestest_qsort(Smaller, Equal ++ bestest_qsort(Larger, Acc));
+bestest_partition(Pivot, [H|T], {Smaller, Equal, Larger}, Acc) ->
+  if H < Pivot ->
+    bestest_partition(Pivot, T, {[H|Smaller], Equal, Larger}, Acc);
+    H > Pivot ->
+      bestest_partition(Pivot, T, {Smaller, Equal, [H|Larger]}, Acc);
+    H == Pivot ->
+      bestest_partition(Pivot, T, {Smaller, [H|Equal], Larger}, Acc)
+  end.
